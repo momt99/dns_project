7f7f65fc3f3185cba5098fea9438a480b6e93b7f
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2021-07-14 17:58:51 +0430

    use built-in features for identify algorithms.

--- certificate_authority/validator.py
+++ certificate_authority/validator.py
@@ -1,5 +1,6 @@
 from cryptography.exceptions import InvalidSignature
 from cryptography.x509 import *
+from cryptography.hazmat.primitives.asymmetric import padding
 
 from utils.signing import get_default_signing_padding, get_default_hash
 
@@ -9,8 +10,8 @@ def verify_certificate(certificate: Certificate):
         cert = load_pem_x509_certificate(f.read())
 
     try:
-        cert.public_key().verify(certificate.signature, certificate.tbs_certificate_bytes,
-                                 get_default_signing_padding(), get_default_hash())
+        cert.public_key().verify(certificate.signature, certificate.tbs_certificate_bytes, padding.PKCS1v15(),
+                                 certificate.signature_hash_algorithm)
         return True
     except InvalidSignature:
         return False
