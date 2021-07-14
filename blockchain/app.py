ac4b3a40c385c5390d3cb50210da24294d6911c2
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2021-07-14 20:25:25 +0430

    exchange body completed.

--- blockchain/app.py
+++ blockchain/app.py
@@ -4,12 +4,17 @@ import requests
 from cryptography import x509
 from cryptography.exceptions import InvalidSignature
 from flask import Flask, request
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import utils
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
 from certificate_authority.csrgenerator import create_csr
 from certificate_authority.validator import verify_certificate
 from utils.auth import verify_auth_header, extract_user_id
 from utils.signing import *
+from utils.encoding import *
+
+import datetime
 
 app = Flask(__name__)
 
@@ -28,9 +33,16 @@ with open('assets/certificate.pem', "w") as f:
 
 # each account has a 'value', 'password' and an array of policies.
 account = dict({})
-account['135702468'] = dict({'value': 100000, 'password': '1234', 'policies': []})
-account['0987654321'] = dict({'value': 20000, 'password': '4321', 'policies': []})
-# policies is a tuple of (bank_id, policy)
+account['135702468'] = dict(
+    {'value': 100000, 'password': '1234', 'policies': [], 'last_payment': datetime.datetime.utcnow()})
+account['0987654321'] = dict(
+    {'value': 20000, 'password': '4321', 'policies': [], 'last_payment': datetime.datetime.utcnow()})
+
+each_dollar_how_much_crypto = 200
+
+
+def permit_transaction(bank_id, amount):
+    pass
 
 
 @app.route('/exchange', methods=['POST'])
@@ -45,7 +57,26 @@ def exchange():
             return 'Signature Not Match', 450
         if not verify_certificate(certificate):
             return "Invalid Certificate", 451
-        # todo: check value and bank id by policy.
+        amount = int(data['amount'])
+        chosen_hash = hashes.SHA256()
+        hasher = hashes.Hash(chosen_hash)
+        hasher.update(str.encode(str(amount)))
+        digest = hasher.finalize()
+        amount_signature = from_base64(data['amount_signature'])
+        try:
+            certificate.public_key().verify(amount_signature, digest, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                                                                                  salt_length=padding.PSS.MAX_LENGTH),
+                                            utils.Prehashed(chosen_hash))
+        except InvalidSignature:
+            return 'Amount signature Not Match', 452
+        bank_id = extract_user_id(header)
+        user_id = data['user id']
+        if permit_transaction(bank_id, amount):
+            account[user_id]['value'] -= amount * each_dollar_how_much_crypto
+            return "Successful payment", 201
+        else:
+            return "Permission Denied", 403
+
     except ValueError:
         return "Bad create account data", 400
 
